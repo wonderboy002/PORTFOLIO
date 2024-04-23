06bf947e5a05d707594e477ee368bfa187776860
wonderboy002 <nipunkup@gmail.com>
2024-04-23 10:50:29 +0530

    Just a minor chnage

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Navbar from './Components/Navbar'
 import Home from './Components/Home'
 import About from './Components/About'
+import Skills from './Components/Skills'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
      <Navbar/>
      <Home/>
      <About/>
+     <Skills/>
     </>
   )
 }
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Skills = () => {
   return (
     <div>
-      
+      These are the technologies that i have worked with
     </div>
   );
 }
